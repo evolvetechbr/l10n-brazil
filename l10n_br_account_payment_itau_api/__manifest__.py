41f7ba996a98d630070aeb5d5a830bbafa0ac8b9
Marcos Tolentino <marcos.tolentino@gmail.com>
2026-01-20 01:39:22 +0000

    update

--- l10n_br_account_payment_itau_api/__manifest__.py
+++ l10n_br_account_payment_itau_api/__manifest__.py
@@ -16,6 +16,7 @@
     "data": [
         "security/ir.model.access.csv",
         "data/l10n_br_payment_method_itau_api.xml",
+        "data/cnab_codes/banco_itau_api_boleto_instructon.xml",
         "data/cnab_codes/banco_itau_api_boleto_fee_code.xml",
         "data/cnab_codes/banco_itau_api_boleto_protest_code.xml",
         "views/account_journal.xml",
